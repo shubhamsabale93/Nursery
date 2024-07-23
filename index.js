a560f29dec24b2641112977d5b1e609329571023
shubhamsabal_93 <shubhamsabale4733@gmail.com>
2024-07-23 22:56:01 +0530

    paths

--- index.js
+++ index.js
@@ -199,6 +199,16 @@ app.delete("/plant/:id",(req,res)=>{
 })
 
 
+app.use("*",(req,res)=>{
+    res.send(`<div>
+        <h1 style="text-align:center">404 Not Found</h1>
+        
+        </div>`
+    
+    )
+})
+
+
 
 
 const PORT=process.env.PORT
